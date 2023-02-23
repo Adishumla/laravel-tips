869385357b314e5ec3514f9de0497dc8ea06a21e
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-02-23 19:48:04 +0100

    added tailwind and some styling

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,37 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body>
+
+
 <?php
 $user = Auth::user();
-
-echo "Welcome, " . $user->name . "!";
-
 $prices = DB::table("prices")->get();
 $categories = DB::table("category")->get();
 $user_id = Auth::user()->id;
 ?>
+<div class="mx-auto m-4">
+<p class="text-lg">Welcome, {{ Auth::user()->name }}!</p>
+<a href="/logout" class="inline-block py-2 px-4 border border-gray-400 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200">Logout</a>
+</div>
+<h1 class="text-3xl text-center">Add a Resturant</h1>
 
-<a href="/logout">Logout</a>
-<br>
-
-<h1>Enter your resturant!</h1>
-<form action="/resturant" method="POST">
-    @csrf
-    <div class="form-group">
-        <label for="category_id">Category</label>
-        <select name="category_id" id="category_id" class="form-control">
+    <form action="/resturant" method="POST" class="max-w-lg mx-auto">
+        @csrf
+        <div class="mb-4">
+          <label for="category_id" class="block font-bold mb-2">Category</label>
+          <select name="category_id" id="category_id" class="form-select block w-full py-2 px-3 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent">
             @foreach ($categories as $category)
-            <option value="{{ (int) $category->id }}">{{ $category->name }}</option>
+              <option value="{{ (int) $category->id }}">{{ $category->name }}</option>
             @endforeach
-        </select>
+          </select>
+        </div>
         <input type="hidden" name="user_id" value={{ $user_id }}>
-        <label for="price_id"></label>
-        <select name="price_id" id="price_id" class="form-control">
+        <div class="mb-4">
+          <label for="price_id" class="block font-bold mb-2">Price</label>
+          <select name="price_id" id="price_id" class="form-select block w-full py-2 px-3 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent">
             @foreach ($prices as $price)
-                <option value={{ (int) $price->id }}>{{ $price->price }}</option>
+              <option value="{{ (int) $price->id }}">{{ $price->price }}</option>
             @endforeach
-        </select>
-        <label for="name">Name</label>
-        <input type="text" name="name" id="name" class="form-control">
-        <label for="city">city</label>
-        <input type="text" name="city" id="city" class="form-control">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+          </select>
+        </div>
+        <div class="mb-4">
+          <label for="name" class="block font-bold mb-2">Name</label>
+          <input type="text" name="name" id="name" class="form-input block w-full py-2 px-3 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent">
+        </div>
+        <div class="mb-4">
+          <label for="city" class="block font-bold mb-2">City</label>
+          <input type="text" name="city" id="city" class="form-input block w-full py-2 px-3 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent">
+        </div>
+        <div class="mt-6">
+          <button type="submit" class="inline-block py-2 px-4 border border-gray-400 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200">
+            Submit
+          </button>
+        </div>
+    </form>
+
+
+@vite('resources/js/app.js')
+</body>
+</html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,43 +1,59 @@
-<form method="post" action="/login">
-    <div>
-        <label for="email">Email</label>
-        <input name="email" id="email" type="email" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body class="">
+    <?php
+    $resturants = DB::table("resturants")->get();
+    $categories = DB::table("category")->get();
+    $prices = DB::table("prices")->get();
+    $users = DB::table("users")->get();
+    ?>
+
+    <div class="flex  justify-start items-start">
+        <form method="post" action="/login" class="col-md-6 offset-md-3 m-2">
+            <div class="mb-4">
+                <label for="email" class="block text-gray-700 font-bold mb-2">Email</label>
+                <input name="email" id="email" type="email" class="form-input block w-full py-2 px-3 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent" />
+            </div>
+            <div class="mb-6">
+                <label for="password" class="block text-gray-700 font-bold mb-2">Password</label>
+                <input name="password" id="password" type="password" class="form-input block w-full py-2 px-3 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent" />
+            </div>
+            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+            <button type="submit" class="px-4 py-2 bg-gray-800 text-white rounded-md hover:bg-gray-700 w-full">Login</button>
+        </form>
     </div>
-    <div>
-        <label for="password">Password</label>
-        <input name="password" id="password" type="password" />
-    </div>
-    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-    <button type="submit">Login</button>
-</form>
 
 {{-- @include('errors') --}}
+<section class="flex flex-wrap justify-center">
+    @foreach ($resturants as $resturant)
+    {{-- limit 4 divs --}}
+      <div class="bg-white border border-5 shadow-lg rounded-md p-4 m-2 w-64 flex flex-col justify-between">
+        @foreach ($users as $user)
+          @if ($resturant->user_id == $user->id)
+            <h3 class="text-2xl font-bold mb-2">{{ $user->name }}</h3>
+          @endif
+        @endforeach
+        @foreach ($categories as $category)
+          @if ($resturant->category_id == $category->id)
+            <h2 class="text-xl font-semibold mb-2">{{ $category->name }}</h2>
+          @endif
+        @endforeach
+        <h4 class="text-lg font-semibold mb-2">{{ $resturant->name }}</h4>
+        <h5 class="text-lg font-semibold mb-2">{{ $resturant->city }}</h5>
+        <br>
+      </div>
+    @endforeach
+</section>
 
-<?php
-$resturants = DB::table("resturants")->get();
-$categories = DB::table("category")->get();
-$prices = DB::table("prices")->get();
-$users = DB::table("users")->get();
-?>
+    @vite('resources/js/app.js')
 
-@foreach ($resturants as $resturant)
-    @foreach ($users as $user)
-        @if ($resturant->user_id == $user->id)
-            <h1>{{ $user->name }}</h1>
-        @endif
-    @endforeach
-    @foreach ($categories as $category)
-        @if ($resturant->category_id == $category->id)
-            <h2>{{ $category->name }}</h2>
-        @endif
-    @endforeach
-    @foreach ($prices as $price)
-        @if ($resturant->price_id == $price->id)
-            <h3>{{ $price->price }}</h3>
-        @endif
-    @endforeach
-    <h4>{{ $resturant->name }}</h4>
-    <h5>{{ $resturant->city }}</h5>
-    <br>
-@endforeach
+</body>
+</html>
 
